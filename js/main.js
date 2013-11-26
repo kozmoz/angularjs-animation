5af29e7cc6843303946980e8f7cf01c1fed6fc3c
Rob Juurlink <github@juurlink.org>
2013-11-26 23:49:23 +0100

    Removed AngularJS directives, they have nothing to do with animations.

--- js/main.js
+++ js/main.js
@@ -18,37 +18,3 @@ app.directive('onEsc', function () {
         });
     };
 });
-
-app.directive('myPanel', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/template/my-panel.html',
-        // Wrap content.
-        transclude: true,
-        scope: {
-            title: '=',
-            subtitle: '='
-        }
-    };
-});
-
-app.directive('mySlidingPanel', function () {
-    return {
-        require: '^myPanel',
-        restrict: 'E',
-        templateUrl: 'js/template/my-sliding-panel.html',
-        // Wrap content.
-        transclude: true,
-        scope: {
-            // Exposes an API for binding to behaviors.
-            // Binds close() function to on-close attribute.
-            'close': '&onClose'
-        },
-        controller: function ($scope, $log) {
-            $scope.closeSlidingPanel = function () {
-                // Forward to registered on-close() event listeners.
-                $scope.close();
-            };
-        }
-    };
-});
